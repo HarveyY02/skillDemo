5e523b42697876b11ea1547c295742ae44f01cc8
HarveyY02 <r6yang@ucsd.edu>
2022-05-01 23:28:15 -0700

    lib added

---
+++ SkillDemo.java
@@ -0,0 +1,6 @@
+public class SkillDemo {
+    public static int Multiply(int a, int b) {
+        return a * b;
+    }
+
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiply() {
+        assertEquals(5, SkillDemo.Multiply(2, 3));
+    }
+    
+}
+
